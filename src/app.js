717dc9013bcf1f044cf5c6a8584a9ab654fdcb63
dyyllaann <dylan.arveson@gmail.com>
2021-12-07 23:15:37 -0800

    Introduced header module Established basic style rules for header module Added npm server support

---
+++ src/app.js
@@ -0,0 +1,6 @@
+export default function app() {
+  // Create content div
+  var content = document.createElement("div");
+  content.id = "content";
+  document.body.appendChild(content);
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import "./style.css";
+import header from "./header.js";
+import app from "./app.js";
 
+app();
+header();
 console.log("SUCCESS.");
