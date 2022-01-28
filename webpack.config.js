03d2c59b3f69ee1c54ae36f66c490c5d613aff8b
dyyllaann <dylan.arveson@gmail.com>
2022-01-27 20:16:35 -0800

    Change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    // clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
+	mode: "development",
+	entry: "./src/index.js",
+	devtool: "inline-source-map",
+	devServer: {
+		static: "./dist",
+	},
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		publicPath: "/dist/"
+		// filename: "[name].[chunkhash].js",
+		// clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
 };
